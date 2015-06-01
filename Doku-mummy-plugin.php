c80767a4eb8c9cd239c15dd99336b3a8721520f1
KratBro <urbans.ky@web.de>
2015-06-01 21:43:11 +0200

    Login redirect implementiert Jetzt wird jeder nicht eingeloggte User auf die Loginseite gebracht. Dies passiert so lange, bis er sich einlogged.

--- Doku-mummy-plugin.php
+++ Doku-mummy-plugin.php
@@ -21,14 +21,23 @@ require_once('Administration/RoleManager.php');
  * */
 register_activation_hook(__FILE__,  'register_roles');
 
-/**
- * Registriert die Custom Roles
- */
-function register_roles() {
 
+function register_roles() {
 	new RoleManager();
 	//Entfernt die Customeroles.
 	/*remove_role('dokuUser');
 	remove_role('dokuModerator');
 	remove_role('dokuAdmin');*/
+}
+https://codex.wordpress.org/Function_Reference/auth_redirect
+
+add_action('template_redirect', 'redirect_not_logged_in_users');
+/**
+ *  Wenn ein Benutzer nicht eingelogged ist, wird er auf die Loginpage redirected.
+ *  Der Test auf is_user_logged_in ist wichtig, da sonst immer auf die Loginpage verviesen wird.
+ */
+function redirect_not_logged_in_users() {
+	if ( ! is_user_logged_in() ) {
+		auth_redirect();
+	}
 }
