a0ed5dbe4ed18a7bfbe7fb54b3db41337ae62e6e
KratBro <urbans.ky@web.de>
2015-06-01 19:17:05 +0200

    Zufügen der Custom Rollen und entfernen der WP_Rollen

--- Doku-mummy-plugin.php
+++ Doku-mummy-plugin.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Doku Mummy Plugin
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
@@ -11,4 +10,25 @@ License: A "Slug" license name e.g. GPL2
 */
 
 
-echo "Hallo World";
+require_once('Administration/RoleManager.php');
+
+/*
+ * Anscheinend können neue Rollen nur mit Lösungen, die ich nicht kenne, aus Dateien, die nicht die "PluginDatei" sind
+ * gesetzt werden. Daher muss dieser Teil in dieser Datei bleiben.
+ *
+ * register_activation_hook aktiviert sich, wenn das Plugin installiert wird.
+ *
+ * */
+register_activation_hook(__FILE__,  'register_roles');
+
+/**
+ * Registriert die Custom Roles
+ */
+function register_roles() {
+
+	new RoleManager();
+	//Entfernt die Customeroles.
+	/*remove_role('dokuUser');
+	remove_role('dokuModerator');
+	remove_role('dokuAdmin');*/
+}
