71a5017efbcce8cb341445be255050f8c6201e33
KratBro <urbans.ky@web.de>
2015-06-17 18:16:52 +0200

    sphinx progammlogik

---
+++ Sphinx/SphinxDocument.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created for doku_mummy-plugin.
+ * User: Jan
+ * Date: 17.06.2015
+ * Time: 17:55
+ */
+
+class SphinxDocument {
+    /**
+     * Sphinx(exe) Path
+     * @var string
+     */
+    private $sphinx_file_path = "";
+    /**
+     * Abschnitte des Documents.
+     * @var array
+     */
+    private $aAbschnitte = array();
+
+    public function __construct(){
+
+    }
+
+    /**
+     * @param {SphinxDocument} $abschnitt
+     */
+    public function addAbschnitt($abschnitt){
+
+    }
+
+    /**
+     * @param {SphinxDocument} $abschnitt
+     */
+    public function removeAbschnitt($abschnitt){
+
+    }
+
+
+    private function deleteAbschnitt(){
+        
+    }
+
+
+
+
+}
