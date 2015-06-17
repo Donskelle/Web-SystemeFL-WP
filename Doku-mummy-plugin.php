d9d2e25cedea61fe2d31f241572e58ba470c0c94
KratBro <urbans.ky@web.de>
2015-06-17 11:13:19 +0200

    Alle Default Widgets werden jetzt autmatisch entfernt

--- Doku-mummy-plugin.php
+++ Doku-mummy-plugin.php
@@ -117,3 +117,27 @@ function buildRoleGUI($role){
             break;
     }
 }
+
+
+/**
+ * Löscht alle default widgets.
+ *
+ * Quelle: http://www.paulund.co.uk/how-to-remove-default-wordpress-widgets
+ */
+function remove_default_widgets(){
+	unregister_widget('WP_Widget_Pages');
+	unregister_widget('WP_Widget_Calendar');
+	unregister_widget('WP_Widget_Archives');
+	unregister_widget('WP_Widget_Links');
+	unregister_widget('WP_Widget_Meta');
+	unregister_widget('WP_Widget_Search');
+	unregister_widget('WP_Widget_Text');
+	unregister_widget('WP_Widget_Categories');
+	unregister_widget('WP_Widget_Recent_Posts');
+	unregister_widget('WP_Widget_Recent_Comments');
+	unregister_widget('WP_Widget_RSS');
+	unregister_widget('WP_Widget_Tag_Cloud');
+	unregister_widget('WP_Nav_Menu_Widget');
+}
+
+add_action('widgets_init', 'remove_default_widgets', 11);
