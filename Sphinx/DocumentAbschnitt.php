1bdd8dadb5020ef9719bc25c5f2f01a112dd7d1a
KratBro <urbans.ky@web.de>
2015-06-18 20:31:12 +0200

    preparation Sphinx einbingund

---
+++ Sphinx/DocumentAbschnitt.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created for doku_mummy-plugin.
+ * User: Jan
+ * Date: 18.06.2015
+ * Time: 17:50
+ */
+
+
+/**
+ *
+ * Class DocumentAbschnitt
+ *
+ *
+ * Stellt einen Abschnitt eines Sphinxprojekts dar.
+ *
+ */
+class DocumentAbschnitt {
+
+
+    public function __construct(){
+
+    }
+
+
+
+
+
+
+}
